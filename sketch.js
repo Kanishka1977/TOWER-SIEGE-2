ea08b87068b1908f87f4bbcaf6277f2bc873c2c4
Kanishka1977 <63718210+Kanishka1977@users.noreply.github.com>
2020-09-05 16:44:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var stand;
+var block1,block2,block3,block4,block5,block6,block7,block8,block9;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  stand= new Ground(400, 200, 50, 50);
+
+  block1= new Box(330,235,30,40);
+  block2= new Box(360,235,30,40);
+  block3= new Box(390,235,30,40);
+  block4= new Box(420,235,30,40);
+  block5= new Box(450,235,30,40);
+  block6= new Box(360,195,30,40);
+  block7= new Box(390,195,30,40);
+  block8= new Box(420,195,30,40);
+  block9= new Box(390,155,30,40);
+
+
+}
+
+function draw() {
+  background(255,255,255);
+  Engine.update(engine);
+  strokeWeight(4);
+
+  stand.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+
+
+  drawSprites();
+}
